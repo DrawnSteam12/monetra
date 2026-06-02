8c224a323932a3162148d5a7342d0683822b9803
LAPTOP-4EIR2EM4\echo <jayrechospencerisraelb@gmail.com>
2026-06-02 08:19:08 +0800

    Complete responsive foundation setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 function App() {
-  return <h1>Monetra</h1>;
+  return (
+    <div className="app-container p-lg">
+      <div className="flex-center">
+        <h1>Monetra</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import "./assets/css/reset.css";
+import "./assets/css/global.css";
+import "./assets/css/responsive.css";
+import "./assets/css/themes.css";
+
 import App from "./App";
 
 ReactDOM.createRoot(
