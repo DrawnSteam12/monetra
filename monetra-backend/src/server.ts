6510efc720f94f1a9832d09002f068680602a5c9
LAPTOP-4EIR2EM4\echo <jayrechospencerisraelb@gmail.com>
2026-07-26 14:55:10 +0800

    Update API URL for production

--- monetra-backend/src/server.ts
+++ monetra-backend/src/server.ts
@@ -26,7 +26,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://monetra-mu48-rho.vercel.app"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
